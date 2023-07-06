c17f9e9568cfbd51b5c4df32773bad1ea28f13cb
rmidhunraja@gmail.com <rmidhunraja@gmail.com>
2023-07-06 07:29:35 +0000

    first commit

---
+++ sample_notebook/Untitled Notebook 2023-07-06 12_49_34.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+print("start")
+
+# COMMAND ----------
+
+def sample():
+  print("this is sample function")
+
+# COMMAND ----------
+
+def my_sum(x,y):
+  return x+y
+
+def my_diff(x,y):
+  return x-y
+
+# COMMAND ----------
+
+
